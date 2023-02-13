4d43995abaf9c3d3a340d14df70154a668abcf84
Scziskh <muzychukserhii@gmail.com>
2023-02-13 21:07:50 +0200

    hash-input with button to copy hash

--- frontend/src/components/form/hash-input.jsx
+++ frontend/src/components/form/hash-input.jsx
@@ -1,19 +1,40 @@
-import { createSignal } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
 import { styled } from "solid-styled-components";
+import icon from "../../assets/copy-ico.svg";
 
 const HashInput = (props) => {
-  const [value, setValue] = createSignal(props.defaultValue);
+  const [value, setValue] = createSignal(
+    props.defaultValue || `djkbfsdjkbfsdj`
+  );
+  const [isValue, setIsValue] = createSignal(Boolean(props.defaultValue));
+
+  createEffect(() => {
+    setIsValue(Boolean(value()));
+  });
+
+  // copy value of field to clickboard
+  const copyToClickboard = () => {
+    navigator.clipboard.writeText(value());
+  };
+
+  // freeze value in input
+  const freezeInput = (e) => {
+    e.target.value = value();
+  };
+
   return (
     <Wrapper>
       <Field
+        type={`text`}
         placeholder={` `}
         id={props.name}
         name={props.name}
-        onChange={(e) => setValue(e.target.value)}
+        onChange={freezeInput}
         value={value()}
-        disabled
+        disabled={!isValue()}
       />
       <Label for={props.name}>{props.placeholder}</Label>
+      <Image src={icon} visibility={isValue()} onClick={copyToClickboard} />
     </Wrapper>
   );
 };
@@ -25,17 +46,18 @@ const Wrapper = styled("fieldset")``;
 const Field = styled("input")`
   border: 2px solid var(--mainColor);
   border-right: none;
-  &::after {
-    content: "copy";
-    height: 38px;
-    width: 38px;
-    display: block;
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
+  padding-left: 40px;
 `;
 
 const Label = styled("label")`
   padding-top: 2px;
+`;
+
+const Image = styled("img")`
+  cursor: pointer;
+  visibility: ${(props) => (props.visibility ? "visible" : "hidden")};
+  position: absolute;
+  width: 20px;
+  top: 10px;
+  left: 10px;
 `;
