8156667a220f178819870010697e1678e2e1409b
Scziskh <muzychukserhii@gmail.com>
2023-02-15 20:03:11 +0200

    env in vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
-export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: "esnext",
-  },
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [solidPlugin()],
+    server: {
+      port: parseInt(process.env.VITE_PORT),
+    },
+    build: {
+      target: "esnext",
+    },
+  });
+};
