d327011e08d21fa2984cd3cf88be8bb94b899c8a
scziskh <muzychukserhii@gmail.com>
2023-03-26 11:07:40 +0300

    rename getValue to updateValueLS

--- frontend/src/helpers/index.js
+++ frontend/src/helpers/index.js
@@ -10,7 +10,7 @@ export const validateEmail = (value) =>
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   );
 
-export const updateValueLS = (name, defaultValue) => {
+export const getValueLS = (name, defaultValue) => {
   const value = localStorage.getItem(name);
   if (value) {
     return value;
