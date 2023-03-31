d3fb6cba2eac58076dd4c721c6c4ac8286d2c9e7
scziskh <muzychukserhii@gmail.com>
2023-03-31 23:08:05 +0300

    init branch frontend/edit-page

--- frontend/src/routes/edit.jsx
+++ frontend/src/routes/edit.jsx
@@ -1,5 +1,12 @@
+import { styled } from "solid-styled-components";
+
 const Edit = () => {
-  return <div>Edit page</div>;
+  return <Wrapper>Edit page</Wrapper>;
 };
 
 export default Edit;
+
+const Wrapper = styled("section")`
+  display: flex;
+  gap: var(--outsidePadding);
+`;
