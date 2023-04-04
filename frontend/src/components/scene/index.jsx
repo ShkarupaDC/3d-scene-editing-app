7f04b56fc57ef52a0bb0f1e0d57edde91fc6706a
Scziskh <muzychukserhii@gmail.com>
2023-04-04 21:04:31 +0300

    three js experiments

--- frontend/src/components/scene/index.jsx
+++ frontend/src/components/scene/index.jsx
@@ -1,14 +1,17 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { PLYLoader } from "three/examples/jsm/loaders/PLYLoader";
 
 const Scene = () => {
+  const loader = new PLYLoader();
+
   // creating scene
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xe5e5e5);
+  scene.background = new THREE.Color(0xa0a0a0);
 
   // creating camera
   const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 256);
-  camera.position.set(10, 10, 10);
+  camera.position.set(0, 0, 5);
   camera.lookAt(0, 0, 0);
 
   // creating renderer
@@ -19,45 +22,22 @@ const Scene = () => {
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.update();
 
-  // add 3D object to scene
-  // const cubeGeometry = new THREE.BoxGeometry(128, 128, 128);
-  // const cubeMaterial = new THREE.MeshNormalMaterial();
-  // const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  // scene.add(cube);
-
-  // grid
-
-  const gridGeometry = new THREE.BoxGeometry(1, 1, 1);
-  const gridMaterial = new THREE.LineDashedMaterial({
-    color: 0xffffff,
-    linewidth: 1,
-    scale: 1,
-    dashSize: 1,
-    gapSize: 1,
-  });
-  const grid = new THREE.InstancedMesh(
-    gridGeometry,
-    gridMaterial,
-    Math.pow(10, 3)
-  );
-
-  let i = 0;
-
-  const matrix = new THREE.Matrix4();
-
-  for (let x = 0; x < 10; x++) {
-    for (let y = 0; y < 10; y++) {
-      for (let z = 0; z < 10; z++) {
-        matrix.setPosition(5 - x, 5 - y, 5 - z);
-
-        grid.setMatrixAt(i, matrix);
-
-        i++;
-      }
+  loader.load(
+    "/meshes/lego_hybrid_5.ply",
+    function (geometry) {
+      console.log(geometry);
+      const material = new THREE.MeshNormalMaterial();
+      const mesh = new THREE.Mesh(geometry, material);
+      scene.add(mesh);
+      mesh.rotateX(-Math.PI / 2);
+    },
+    (xhr) => {
+      console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+    },
+    (error) => {
+      console.log(error);
     }
-  }
-
-  scene.add(grid);
+  );
 
   // callback update dom and return it
   const getThreeDom = () => {
