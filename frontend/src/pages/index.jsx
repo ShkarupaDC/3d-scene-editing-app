de968b7f23742834e2794e264581fc9251d72bfe
Dmytro Shkarupa <dmytro.shkarupa@Dmytro.Shkarupa>
2023-05-09 16:58:35 +0300

    Add Dmytro tasks

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -5,7 +5,11 @@ import ComposeSection from '../components/sections/compose-section';
 import RenderingSection from '../components/sections/rendering-section';
 import EmailSection from '../components/sections/email-section';
 
-// 1. Error handling
+// 1. Test everything
+// 2. Responsive canvases
+// 3. TransformControls for mesh in AABB viewer
+// 4. Add file with cameras per object on backend
+// 5. Send mask filenames to backend
 
 const IndexPage = () => {
   return (
